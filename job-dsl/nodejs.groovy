6558c4385fb344f6163eb41df411a846d257cd10
Kaushik Das <kaushik.das@codeclouds.in>
2022-09-16 17:27:40 +0530

    updated node.js.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('https://github.com/ikaushikdas/simple-node-js-react-npm-app.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('Kaushik Das')
+            node / gitConfigEmail('kaushik.das@codeclouds.in')
         }
     }
     triggers {
