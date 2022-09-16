4bef0e1ed662e62c3a313a67d98ce19c464e6099
Kaushik Das <kaushik.das@codeclouds.in>
2022-09-17 01:24:13 +0530

    ..

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -17,15 +17,3 @@ job('Node Job-dSL EXample') {
     }
     
 }
-job('Node docker EXample') {
-steps {
-        dockerBuildAndPublish {
-            repositoryName('ikaushik96/nodeapp')
-            tag('${BUILD_TIMESTAMP}-${GIT_REVISION,length=7}')
-            registryCredentials('docker-hub')
-            forcePull(false)
-            createFingerprints(false)
-            skipDecorate()
-        }
-    }
-}
